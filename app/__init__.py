eabc42443fe0c38de7e0134e5a8f16e22ec6fed0
alexkarari <k.alexander@gmail.com>
2018-04-07 20:09:22 +0300

    init.py
    importing config from config_options error resolved

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from .config import config_options
+from config import config_options
 
 bootstrap = Bootstrap()
 
@@ -24,3 +24,5 @@ def create_app(config_name):
     configure_request(app)
 
     return app
+
+
